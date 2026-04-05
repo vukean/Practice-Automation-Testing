d1df16b98953b2eda537fa87ead0b5771d3b7bc5
Vu Ke An <anvuke2001@gmail.com>
2026-04-05 17:12:49 +0700

    Check radio Button

--- tests/Automationpractices.spec.ts
+++ tests/Automationpractices.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
 
 test('Test input Field', async ({ page }) => {
   await page.goto('https://practice.expandtesting.com/inputs');
@@ -103,4 +105,62 @@ test('Login page Invalid passWord',async({page})=>{
   await btnLogin.click();
   //Verify that an error message "Invalid username." is displayed.
   await expect(page.getByText('Your password is invalid!')).toBeVisible();
+})
+test('Test register with faker data', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/register');
+
+  const username = `${faker.person.firstName().toLowerCase()}${faker.number.int({ min: 100, max: 999 })}`;
+  const password = faker.internet.password({
+    length: 4,
+  });
+  await page.getByRole('textbox', { name: 'Username' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password', exact: true }).fill(password)
+  await page.getByRole('textbox', { name: 'Confirm Password' }).fill(password);
+  await page.getByRole('button', { name: 'Register' }).click()
+
+  await expect(page).toHaveURL('https://practice.expandtesting.com/login')
+  // alert/message box
+  await expect(page.locator('#flash')).toContainText('Successfully registered, you can log in now.')
+});
+
+test('Radio button  ', async({page})=>{
+  await page.goto('https://practice.expandtesting.com/radio-buttons')
+  const cBlue = page.getByRole('radio', { name: 'Blue' })
+  const cRed = page.getByRole('radio', { name: 'Red' })
+  const cYeallow = page.getByRole('radio', { name: 'Yellow' })
+  const cBlack = page.getByRole('radio', { name: 'Black' })
+  const sBasketball = page.getByRole('radio', { name: 'Basketball' })
+  const sFootball = page.getByRole('radio', { name: 'Football' })
+  const Tennis = page.getByRole('radio', { name: 'Tennis' })
+
+  //Check color Blue
+  await cBlue.check();
+  await expect(cBlue).toBeChecked();
+  await page.waitForTimeout(200);
+  //Check color Red
+  await cRed.check();
+    await expect(cRed).toBeChecked();
+  await page.waitForTimeout(200);
+  //Check color Yeallow
+  await cYeallow.check();
+      await expect(cYeallow).toBeChecked();
+  await page.waitForTimeout(200);
+  //Check color Black
+  await cBlack.check();
+      await expect(cBlack).toBeChecked();
+  await page.waitForTimeout(200);
+
+  //check Basetball
+    await sBasketball.check();
+      await expect(sBasketball).toBeChecked();
+  await page.waitForTimeout(200);
+  //check Football
+      await sFootball.check();
+      await expect(sFootball).toBeChecked();
+    await page.waitForTimeout(200);
+      //check Tennis
+      await Tennis.check();
+      await expect(Tennis).toBeChecked();
+    await page.waitForTimeout(200);
+
 })
