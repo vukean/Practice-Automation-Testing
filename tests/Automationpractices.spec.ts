007c0e8e4e759c01e584d4e6dbac4d81fbe20158
Vu Ke An <anvuke2001@gmail.com>
2026-04-05 14:55:37 +0700

    Check output Loop each time press ArrowUp and ArrowDown

--- tests/Automationpractices.spec.ts
+++ tests/Automationpractices.spec.ts
@@ -1,12 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test('Input fields should accept values correctly', async ({ page }) => {
+test('Test input Field', async ({ page }) => {
   await page.goto('https://practice.expandtesting.com/inputs');
 
   // Input Number
-  const numberInput = page.locator('input[type="number"]');
-  await numberInput.fill('123');
-  await expect(numberInput).toHaveValue('123');
+
+  const numberInput = page.getByRole('spinbutton', { name: 'Input: Number' })
+  await numberInput.fill('100');
+  await expect(numberInput).toHaveValue('100');
+  
+    // Input Number click arrowup + check each time press arrowUp
+    const numberArrowUp =page.getByRole('spinbutton', { name: 'Input: Number' })
+    await numberArrowUp.press('ArrowUp');
+    await expect(numberArrowUp).toHaveValue('101')
+    for (let i = 0; i < 10; i++) {
+    await numberArrowUp.press('ArrowUp');
+    await page.waitForTimeout(500);
+    await expect(numberArrowUp).toHaveValue(String(102 + i));
+  }
+      await expect(numberArrowUp).toHaveValue('111');
+
+ // Input Number click arrowup + check each time press arrowDown
+    const numberArrowDown =page.getByRole('spinbutton', { name: 'Input: Number' })
+    await numberArrowDown.press('ArrowDown');
+    await expect(numberArrowDown).toHaveValue('110')
+    for (let i = 0; i < 10; i++) {
+    await numberArrowDown.press('ArrowDown');
+    await page.waitForTimeout(500);
+    await expect(numberArrowDown).toHaveValue(String(109 - i));
+  }
+      await expect(numberArrowDown).toHaveValue('100');
 
   // Input Text
   const textInput = page.locator('input[type="text"]').nth(0);
